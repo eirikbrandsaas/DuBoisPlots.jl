62424adec00a4bee73b1e6ed7fb85f0d2be3851f
Documenter.jl <documenter@juliadocs.github.io>
2022-06-15 13:37:14 +0000

    build based on e465cd3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DuBoisPlots","category":"page"},{"location":"#DuBoisPlots","page":"Home","title":"DuBoisPlots","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DuBoisPlots.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DuBoisPlots]","category":"page"}]
+}

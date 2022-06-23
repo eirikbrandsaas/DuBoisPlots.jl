f69f6b5c24a1cec6844089d98cde4667207d40fc
Eirik Eylands Brandsaas <eebrandsaas@gmail.com>
2022-06-23 11:34:55 -0400

    Fixed paths for data

--- src/utilities.jl
+++ src/utilities.jl
@@ -7,17 +7,17 @@ function _Plate10df()
 end
 
 function _Plate11df()
-    CSV.read(joinpath(Base.@__DIR__,"..","data/original/Plate10.csv"),DataFrame)
+    CSV.read(joinpath(Base.@__DIR__,"..","data/original/Plate11.csv"),DataFrame)
 end
 
 function _Plate25df()
-    CSV.read(joinpath(Base.@__DIR__,"..","data/original/Plate10.csv"),DataFrame)
+    CSV.read(joinpath(Base.@__DIR__,"..","data/original/Plate25.csv"),DataFrame)
 end
 
 function _Plate51df()
-    CSV.read(joinpath(Base.@__DIR__,"..","data/original/Plate10.csv"),DataFrame)
+    CSV.read(joinpath(Base.@__DIR__,"..","data/original/Plate51.csv"),DataFrame)
 end
 
 function _Plate53df()
-    CSV.read(joinpath(Base.@__DIR__,"..","data/original/Plate10.csv"),DataFrame)
+    CSV.read(joinpath(Base.@__DIR__,"..","data/original/Plate53.csv"),DataFrame)
 end
